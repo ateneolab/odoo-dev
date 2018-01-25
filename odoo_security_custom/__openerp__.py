1fc26023f06069b643ec40b4c80f0f4f9f8cc206
soint\soaint <ymatos3@gmail.com>
2018-01-25 01:53:20 -0500

    Ajustes en manifest

--- odoo_security_custom/__openerp__.py
+++ odoo_security_custom/__openerp__.py
@@ -10,22 +10,23 @@
         Long description of module's purpose
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Solem Consulting",
+    'website': "www.solemconsulting.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Security',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
+        'views/account_invoice_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [

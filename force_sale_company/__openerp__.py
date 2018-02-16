4d0cd2ed536268eff89f2935ec6f50fa1d57efb8
soint\soaint <ymatos3@gmail.com>
2018-02-16 01:39:25 -0500

    ...

--- force_sale_company/__openerp__.py
+++ force_sale_company/__openerp__.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "force_sale_company",
+    'name': "Force Sale Company",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Set company_id to main company and warehouse_id to default warehouse of the main company.""",
 
     'description': """
-        Long description of module's purpose
+        If you delete default warehouse of the main company, this moduel should break.
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Solem Consulting",
+    'website': "http://www.solemconsulting.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
---
+++ force_sale_company/code/sale.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def create(self, vals):
+        vals['company_id'] = 1
+        vals['warehouse_id'] = 1
+
+        return super(sale_order, self).create(vals)

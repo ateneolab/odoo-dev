0f6a9dad2a9521b29066bb84bb7037a48898888a
soint\soaint <ymatos3@gmail.com>
2018-02-18 12:49:12 -0500

    email template recibe el server_id

--- mail_outgoing_server/__openerp__.py
+++ mail_outgoing_server/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mail_outgoing_server",
+    'name': "Mail Outgoing Sserver",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,17 +10,17 @@
         Long description of module's purpose
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Solem Consulting",
+    'website': "www.solemconsulting.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Mail',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail', 'email_template'],
 
     # always loaded
     'data': [

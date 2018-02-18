4e871bbd88e6ca1891f43101fafc5ff7b76da986
soint\soaint <ymatos3@gmail.com>
2018-02-18 13:08:12 -0500

    ...

--- mail_outgoing_server/__openerp__.py
+++ mail_outgoing_server/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Mail Outgoing Sserver",
+    'name': "Mail Outgoing Server",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as

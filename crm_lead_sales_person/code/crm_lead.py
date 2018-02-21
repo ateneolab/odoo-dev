8bb50948d008b190c91d5eafc1b4f36a6ffeac77
soint\soaint <ymatos3@gmail.com>
2018-02-21 01:14:22 -0500

    ...

--- crm_lead_sales_person/code/crm_lead.py
+++ crm_lead_sales_person/code/crm_lead.py
@@ -8,6 +8,12 @@ class crm_lead(models.Model):
 
     @api.model
     def _filter_sales_persons(self):
-        return [('id', 'in', [1, 2, 3])]
+        users = []
+
+        teams = self.env['crm.case.section'].search([])
+        for team in teams:
+            users += team.member_ids.ids
+
+        return [('id', 'in', list(set(users)))]
 
     user_id = fields.Many2one('res.users', domain=_filter_sales_persons)

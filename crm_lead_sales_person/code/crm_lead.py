16983eb1019830d270181e5340fcc9a954aeabeb
soint\soaint <ymatos3@gmail.com>
2018-02-21 00:22:20 -0500

    new filter

--- crm_lead_sales_person/code/crm_lead.py
+++ crm_lead_sales_person/code/crm_lead.py
@@ -5,6 +5,6 @@ from openerp import models, fields, api
 class crm_lead(models.Model):
     _inherit = 'crm.lead'
 
-    @api.onchange('section_id')
-    def onchange_section_id(self):
-        pass
+
+    def filter_sales_persons(self):
+        return [1, 2, 3]

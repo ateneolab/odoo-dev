af04ab220bbbdce751c4a0b8690e7a02fcdd5b09
soint\soaint <ymatos3@gmail.com>
2018-02-21 00:56:10 -0500

    ...

--- crm_lead_sales_person/code/crm_lead.py
+++ crm_lead_sales_person/code/crm_lead.py
@@ -10,4 +10,4 @@ class crm_lead(models.Model):
     def _filter_sales_persons(self):
         return [('id', 'in', [1, 2, 3])]
 
-    user_id = fields.Many2one('res.users', domain=_filter_sales_persons())
+    user_id = fields.Many2one('res.users', domain=_filter_sales_persons)

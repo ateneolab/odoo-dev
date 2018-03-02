d3ffe9f308ec004d7d7717593722a560ec7b711c
soint\soaint <ymatos3@gmail.com>
2018-03-02 00:35:15 -0500

    Uncomment supply_rate

--- fix_discount_type/code/invoice.py
+++ fix_discount_type/code/invoice.py
@@ -71,7 +71,7 @@ class account_type(models.Model):
         total = val1 + val2 - disc_amnt
         self.amount_discount = disc_amnt
 
-    """@api.onchange('discount_type', 'discount_rate')
+    @api.onchange('discount_type', 'discount_rate')
     def supply_rate(self):
         # for inv in self:
         if self.discount_rate != 0:
@@ -87,7 +87,7 @@ class account_type(models.Model):
             if self.discount_rate != 0:
                 if total != 0.0:
                     discount = (self.discount_rate / total) * 100
-            self.compute_discount(discount)"""
+            self.compute_discount(discount)
 
             # raise osv.except_osv('Error en detalles', u'Especifique todos los precios y las cantidades de los detalles de la factura.')
 

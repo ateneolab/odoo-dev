fc9995ba247f1c1bc9e7c90317055f2082ab39c0
soint\soaint <ymatos3@gmail.com>
2018-03-28 12:46:05 -0500

    fix

--- education_contract/models.py
+++ education_contract/models.py
@@ -667,8 +667,8 @@ class plan(models.Model):
 class payment_info(models.Model):
     _name = 'education_contract.payment_info'
 
+    @api.one
     @api.onchange('payment_term_ids', 'amount', 'plan_id')
-    @api.depends('payment_term_ids', 'amount', 'plan_id')
     def _compute_residual(self):
         sum = 0.0
 

5fd0d4ab834d05cec007a268f9e5d91083426541
soint\soaint <ymatos3@gmail.com>
2018-04-08 20:21:10 -0500

    Pass OP to Contract after create from Sale order.

--- education_contract/sale.py
+++ education_contract/sale.py
@@ -8,26 +8,26 @@ from dateutil import parser
 class sale_order(models.Model):
     _name = 'sale.order'
     _inherit = 'sale.order'
-    
+
     education_contract_id = fields.Many2one('education_contract.contract', string='Contrato de estudios')
     generate_contract = fields.Boolean(_('Generate contract'), default=False)
 
-
     def action_button_confirm(self, cr, uid, ids, context=None):
         res = super(sale_order, self).action_button_confirm(cr, uid, ids, context=context)
-        
+
         if res and self.generate_contract:
             contract_id = self.generate_education_contract(cr, uid, ids, context=context)
-            
+
             if contract_id:
                 self.pool.get('sale.order').write(cr, uid, ids, {'education_contract_id': contract_id})
-        
-        return res
-    
-    
-    def generate_education_contract(self, cr, uid, ids, context=None):
-        
-        contract_id = self.pool.get('education_contract.contract').create(cr, uid, {'sale_order_id': ids}, context=context)
-        return contract_id
+                self.pool.get('education_contract.contract').write(cr, uid, [contract_id],
+                                                                   {'operating_unit_id',
+                                                                    self.operating_unit_id}, context=context)
 
-        
+        return res
+
+    def generate_education_contract(self, cr, uid, ids, context=None):
+
+        contract_id = self.pool.get('education_contract.contract').create(cr, uid, {'sale_order_id': ids},
+                                                                          context=context)
+        return contract_id

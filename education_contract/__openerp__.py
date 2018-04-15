8124e60c438cf0432aeb626f5d9ebeff6df9fc06
soint\soaint <ymatos3@gmail.com>
2018-04-14 20:08:30 -0500

    Fix

--- education_contract/__openerp__.py
+++ education_contract/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Contrato de Educación",
+    'name': "Educational Contract",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as

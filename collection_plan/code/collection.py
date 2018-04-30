01b33fd7d6189fef9f79cb3f6f06c7c7c08b78f7
soint\soaint <ymatos3@gmail.com>
2018-04-30 05:34:34 -0500

    without payment_term_fixed_ids

--- collection_plan/code/collection.py
+++ collection_plan/code/collection.py
@@ -91,8 +91,10 @@ class EducationContractPlan(models.Model):
 
                 before_date = sd
 
-    payment_term_fixed_ids = fields.One2many('education_contract.payment_term', 'fixed_plan_id',
-                                             compute='_compute_payment_terms', string=_('Payment terms'), store=True)
+    payment_term_ids =          fields.One2many('education_contract.payment_term', 'plan_id',
+                                       string='Formas de pago')
+    payment_term_fixed_ids =    fields.One2many('education_contract.payment_term', 'fixed_plan_id',
+                                             string=_('Payment terms'), store=True)
     collection_plan_id = fields.Many2one('collection_plan.collection_plan', string=_(''))
     plan_active = fields.Boolean(_('Active'))
     balance = fields.Float(digits=(6, 4), compute='_compute_balance', string=_('Balance'))

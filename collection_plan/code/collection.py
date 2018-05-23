11567ac8e06eab3761efc5cfe51450a1f45bb451
soint\soaint <ymatos3@gmail.com>
2018-05-22 22:42:50 -0500

    collection_plan show payed payment terms an not payed payment terms.

--- collection_plan/code/collection.py
+++ collection_plan/code/collection.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api, _
 import datetime
 from datetime import datetime
+
 from dateutil.relativedelta import relativedelta
+
+from openerp import models, fields, api, _
 
 
 class CollectionPlan(models.Model):
@@ -31,7 +33,13 @@ class CollectionPlan(models.Model):
     @api.one
     @api.depends('active_plan_id', 'plan_ids', 'payment_term_ids')
     def _compute_payed_terms(self):
-        pass
+        pts = []
+        for pt in self.active_plan_id.payment_term_ids:
+            if pt.payed:
+                pts.append(pt.id)
+
+        for pt_id in pts:
+            self.payed_payment_term_ids = [(4, pt_id)]
 
     @api.one
     @api.onchange('contract_id')
@@ -54,9 +62,9 @@ class CollectionPlan(models.Model):
 
         new_plan = self.active_plan_id.copy({
             'payment_term_ids': None,
-            'amount_pay': self.active_plan_id.amount_pay,
+            'amount_pay': self.active_plan_id.residual,  # el total a pagar es lo que no se ha pagado hasta el momento
             'qty_dues': 0.0,
-            'amount_monthly': self.active_plan_id.amount_pay,
+            'amount_monthly': self.active_plan_id.residual,
             'registration_fee': 0.0,
             'residual': self.active_plan_id.amount_pay,
             'collection_plan_id': self.id,
@@ -75,9 +83,9 @@ class CollectionPlan(models.Model):
     state = fields.Selection([('created', _('New')), ('done', _('Finish'))], default='created')
     payment_term_ids = fields.One2many(related='active_plan_id.payment_term_fixed_ids',
                                        string=_('Payment terms from active plan'))
-    """payed_payment_term_ids = fields.One2many('education_contract.payment_term', 'payed_collection_plan_id',
-                                         string=_('All payed Payment terms'), compute='_compute_payed_terms',
-                                         store=True)"""
+    payed_payment_term_ids = fields.One2many('education_contract.payment_term', 'payed_collection_plan_id',
+                                             string=_('All payed Payment terms'), compute='_compute_payed_terms',
+                                             store=True)
     user_id = fields.Many2one('res.users', string=_('Account manager'))
     start_date = fields.Date('Start date')
     end_date = fields.Date('End date')
@@ -87,6 +95,8 @@ class CollectionPlan(models.Model):
 class EducationContractPlan(models.Model):
     _name = 'education_contract.plan'
     _inherit = 'education_contract.plan'
+
+    # residual = fields.Float(digits=(10, 4), string=_(u'Residual'), compute='compute_residual')
 
     @api.one
     def remove_payment_terms(self):
@@ -99,11 +109,24 @@ class EducationContractPlan(models.Model):
                 pt.unlink()
 
     @api.one
+    @api.depends('payment_term_ids', 'payment_term_fixed_ids')
+    def compute_residual(self):
+        residual = 0.0
+
+        for pt in self.payment_term_ids:
+            if not pt.payed:
+                residual += 1
+
+        return residual
+
+    @api.one
     def reschedule(self):
         index = 1
         before_date = datetime.strptime(self.start_date, '%Y-%m-%d')
 
         self.remove_payment_terms()
+
+        amount_monthly = self.residual / self.qty_dues
 
         if self.qty_dues and self.plan_active:
             for n in range(1, self.qty_dues + 1):
@@ -113,7 +136,7 @@ class EducationContractPlan(models.Model):
                     sd = before_date + relativedelta(months=+1)
 
                 new_payment_term = self.env['education_contract.payment_term'].create({
-                    'amount': self.amount_monthly,
+                    'amount': amount_monthly,
                     'planned_date': sd,
                     'plan_id': self.id
                 })
@@ -181,8 +204,7 @@ class PaymentTerm(models.Model):
     payed = fields.Boolean(_('Payed?'))
     order = fields.Integer('Order')
     fixed_plan_id = fields.Many2one('education_contract.plan', string=_('Payment Plan'))
-
-    # payed_collection_plan_id = fields.One2many('collection_plan.collection_plan', string=_('Payed Collection Plan'))
+    payed_collection_plan_id = fields.One2many('collection_plan.collection_plan', string=_('Payed Collection Plan'))
 
 
 class EducationContract(models.Model):

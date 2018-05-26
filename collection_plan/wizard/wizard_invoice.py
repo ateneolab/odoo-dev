4eceec2006ecefe27f15506adf9da70da8cd770f
soint\soaint <ymatos3@gmail.com>
2018-05-26 12:39:46 -0500

    fix reference field

--- collection_plan/wizard/wizard_invoice.py
+++ collection_plan/wizard/wizard_invoice.py
@@ -12,7 +12,7 @@ class WizardInvoice(models.TransientModel):
     _name = 'collection_plan.wizard_invoice'
 
     invoice_id = fields.Many2one('account.invoice', string=_(u'Invoices'))
-    payment_term_ids = fields.One2many('education_contract.payment_term', string=_('Payments'))
+    payment_term_ids = fields.One2many('education_contract.payment_term', 'wizard_invoice_id', string=_('Payments'))
     partner_id = fields.Many2one('res.partner', string=_('Customer'))
     operating_unit_id = fields.Many2one('operating_unit_id', string=_('Branch office'))
     company_id = fields.Many2one('res.company', string=_(u'Company'))
@@ -131,3 +131,10 @@ class WizardInvoice(models.TransientModel):
         _logger.info(inv)
 
         ## como cierro el wizard y luego ir directo a la factura creada? ver como lo hace el pedido de venta
+
+
+class PaymentTerm(models.Model):
+    _name = 'education_contract.payment_term'
+    _inherit = 'education_contract.payment_term'
+
+    wizard_invoice_id = fields.Many2one('collection_plan.wizard_invoice')

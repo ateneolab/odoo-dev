768f9826f86a7df7fcffe7938ba11f319f322e0e
soint\soaint <ymatos3@gmail.com>
2018-05-26 23:15:41 -0500

    fix

--- collection_plan/wizard/wizard_invoice.py
+++ collection_plan/wizard/wizard_invoice.py
@@ -19,6 +19,19 @@ class WizardInvoice(models.TransientModel):
     operating_unit_id = fields.Many2one('operating.unit', string=_('Branch office'))
     company_id = fields.Many2one(related='operating_unit_id.company_id', string=_(u'Company'))
 
+    @api.cr_uid_context
+    def fields_get(self, fields=None, context=None, write_access=True, attributes=None):
+        res = super(WizardInvoice, self).fields_get(self._cr, self._uid, fields, context, write_access, attributes)
+
+        import pdb
+        pdb.set_trace()
+
+        # if 'communication2' in res:
+        #     res['communication2'].setdefault('states', {})
+        #     res['communication2']['states']['structured'] = [('readonly', True)]
+        #     res['communication2']['states']['normal'] = [('readonly', False)]
+        return res
+
     @api.multi
     def build_lines(self):
         self.ensure_one()
@@ -134,7 +147,7 @@ class WizardInvoice(models.TransientModel):
         tree_id = tree_res and tree_res[1] or False
 
         return {
-            'name': _('Advance Invoice'),
+            'name': _('Generated Invoice'),
             'view_type': 'form',
             'view_mode': 'form,tree',
             'res_model': 'account.invoice',

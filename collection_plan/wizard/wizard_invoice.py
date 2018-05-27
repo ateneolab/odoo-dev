526143ce1618389efede20ce8112090fb9857fcc
soint\soaint <ymatos3@gmail.com>
2018-05-27 00:46:49 -0500

    -

--- collection_plan/wizard/wizard_invoice.py
+++ collection_plan/wizard/wizard_invoice.py
@@ -14,8 +14,7 @@ class WizardInvoice(models.TransientModel):
 
     contract_id = fields.Many2one('education_contract.contract', 'Contrato')
     # verification_id = fields.Many2one('education_contract.verification', related='contract_id.verification_id')
-    # collection_plan_id = fields.Many2one('collection_plan.collection_plan',
-    #                                      related='verification_id.collection_plan_id')
+    collection_plan_id = fields.Many2one('collection_plan.collection_plan')
 
     invoice_id = fields.Many2one('account.invoice', string=_(u'Invoices'))
     payment_term_ids = fields.Many2many('education_contract.payment_term', 'wizard_invoice_payment_term',

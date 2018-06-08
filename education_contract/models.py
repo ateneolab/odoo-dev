8a53c200a00baddb65fc04ae6cfa2e5c235d15d9
soint\soaint <ymatos3@gmail.com>
2018-06-08 01:18:18 -0500

    fix

--- education_contract/models.py
+++ education_contract/models.py
@@ -134,7 +134,7 @@ class program(models.Model):
         pdb.set_trace()
         courses_id = self.env['op.course'].search([('code', '=', self.name)])
         if courses_id:
-            self.course_id = courses_id.id
+            self.course_id = courses_id
         return courses_id
 
     name = fields.Selection(selection='_get_courses_selection', string='Nombre del Programa')

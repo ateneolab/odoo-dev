4d333e1218c74f49061877ddd5e6c00408468611
Yusnier Matos Arias <ymatos3@gmail.com>
2018-09-02 22:50:50 -0500

    Arreglar el cálculo de los montos en el RPM. Se cambió el filtro de los depósitos en efectivo.

--- education_contract/report/report_parser.py
+++ education_contract/report/report_parser.py
@@ -4,18 +4,19 @@ from openerp.report import report_sxw
 from openerp import api, models
 from openerp.osv import osv
 
+
 class custom_report_parser(report_sxw.rml_parse):
-    
+
     def __init__(self, cr, uid, name, context=None):
-        
+
         super(custom_report_parser, self).__init__(cr, uid, name, context=context)
-        
+
         wizard_pool = self.pool.get('education_contract.rpm_wizard')
         wizard_obj = wizard_pool.browse(self.cr, self.uid, self.localcontext.get('active_id'))
         contract_ids = wizard_obj.contract_ids
-        
+
         advances = self._get_cash_advance(contract_ids)
-        
+
         self.localcontext.update({
             'cash_advance': advances,
             'get_total_cash': self._get_total_cash,
@@ -28,32 +29,31 @@ class custom_report_parser(report_sxw.rml_parse):
             'get_transfers': self._get_transfers,
             'get_checks': self._get_checks,
         })
-    
-    
+
     def _get_cash_deposits(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
-        domain = [('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
+
+        domain = [('type', '=', 'cash'), ('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids),
+                  ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         cash_deposits = []
         for pt in payment_term_obj:
             cash_deposits.append({
                 'date': '',
                 'amount': pt.amount,
             })
-            
+
         return cash_deposits
-        
 
     def _get_transfers(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
+
         domain = [('cash_sub_type', 'in', ['transfer']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         data = []
         for pt in payment_term_obj:
             data.append({
@@ -63,17 +63,16 @@ class custom_report_parser(report_sxw.rml_parse):
                 'auth_number': pt.transfer_id.auth_number,
                 'bank': pt.transfer_id.bank.name
             })
-            
+
         return data
-        
-        
+
     def _get_checks(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
+
         domain = [('type', 'in', ['check']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         data = []
         for pt in payment_term_obj:
             data.append({
@@ -83,18 +82,18 @@ class custom_report_parser(report_sxw.rml_parse):
                 'bank': pt.check_id.bank.name,
                 'beneficiary': pt.check_id.beneficiary
             })
-            
+
         return data
-            
-        
+
     def _get_vouchers(self, docs=None):
-        
+
         plan_ids = [x.plan_id.id for x in docs]
-        
-        domain = ['|', ('type', 'in', ['credit_card']), ('cash_sub_type', 'in', ['debit_card']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
+
+        domain = ['|', ('type', 'in', ['credit_card']), ('cash_sub_type', 'in', ['debit_card']),
+                  ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         vouchers = []
         for pt in payment_term_obj:
             voucher = pt.voucher_id
@@ -106,12 +105,11 @@ class custom_report_parser(report_sxw.rml_parse):
                 'auth_number': voucher.auth_number,
                 'amount': pt.amount,
             })
-            
+
         return vouchers
-    
-    
+
     def _get_cash_advance(self, contract_list=None):
-    
+
         """wizard_pool = self.pool.get('education_contract.rpm_wizard')
         wizard_obj = wizard_pool.browse(self.cr, self.uid, self.localcontext.get('active_id'))
         
@@ -134,14 +132,15 @@ class custom_report_parser(report_sxw.rml_parse):
                 'concept': sa.reason,
                 'amount': sa.advance
             })"""
-            
+
         docs = contract_list
         plan_ids = [x.plan_id.id for x in docs]
-        
-        domain = [('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids), ('state', 'in', ['processed', 'to_advance'])]
+
+        domain = [('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids),
+                  ('state', 'in', ['processed', 'to_advance'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         advances = []
         for pt in payment_term_obj:
             advances.append({
@@ -150,89 +149,84 @@ class custom_report_parser(report_sxw.rml_parse):
                 'concept': pt.salary_advance_id.salary_advance_id.reason,
                 'amount': pt.amount
             })
-            
+
         return advances
-        
-    
+
     def _get_total_cash(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
-        domain = [('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
+
+        domain = [('type', 'in', ['cash']), ('cash_sub_type', 'in', ['cash']), ('plan_id', 'in', plan_ids),
+                  ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         sum = 0.0
-        
+
         for pt in payment_term_obj:
             sum += pt.amount
-            
+
         return sum
-        
-        
+
     def _get_total_check(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
+
         domain = [('type', 'in', ['check']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         sum = 0.0
-        
+
         for pt in payment_term_obj:
             sum += pt.amount
-            
+
         return sum
-        
-        
+
     def _get_total_transfer(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
+
         domain = [('cash_sub_type', 'in', ['transfer']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         sum = 0.0
-        
+
         for pt in payment_term_obj:
             sum += pt.amount
-            
+
         return sum
-        
-        
+
     def _get_total_voucher(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
-        domain = ['|', ('cash_sub_type', 'in', ['debit_card']), ('type', 'in', ['credit_card']), ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
+
+        domain = ['|', ('cash_sub_type', 'in', ['debit_card']), ('type', 'in', ['credit_card']),
+                  ('plan_id', 'in', plan_ids), ('state', 'in', ['done'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         sum = 0.0
-        
+
         for pt in payment_term_obj:
             sum += pt.amount
-            
+
         return sum
-        
-        
+
     def _get_total_advance(self, docs=None):
         plan_ids = [x.plan_id.id for x in docs]
-        
+
         domain = [('type', 'in', ['cash']), ('plan_id', 'in', plan_ids), ('state', 'in', ['to_advance', 'processed'])]
         payment_term_ids = self.pool.get('education_contract.payment_term').search(self.cr, self.uid, domain)
         payment_term_obj = self.pool.get('education_contract.payment_term').browse(self.cr, self.uid, payment_term_ids)
-        
+
         sum = 0.0
-        
+
         for pt in payment_term_obj:
             sum += pt.amount
-            
+
         return sum
-        
+
 
 class report_rpm_parser(osv.AbstractModel):
-    
     _name = 'report.education_contract.report_rpm'
     _inherit = 'report.abstract_report'
     _template = 'education_contract.report_rpm'
     _wrapped_report_class = custom_report_parser
-    

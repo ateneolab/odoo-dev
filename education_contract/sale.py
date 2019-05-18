e028e2bbfc616664a05c743b88ea3074456a84bc
Erich Mario <emgomez@uci.cu>
2019-05-18 08:03:17 -0400

    Refactorizar codigo

--- education_contract/sale.py
+++ education_contract/sale.py
@@ -15,20 +15,34 @@ class sale_order(models.Model):
     def action_button_confirm(self, cr, uid, ids, context=None):
         res = super(sale_order, self).action_button_confirm(cr, uid, ids, context=context)
         sale_o = self.pool.get('sale.order').browse(cr, uid, ids, context=context)
-
         if res and sale_o and sale_o.generate_contract:
-            contract_id = self.generate_education_contract(cr, uid, ids, context=context)
-
+            contract_id = self.generate_education_contract(
+                cr,
+                uid,
+                ids,
+                context=context
+            )
             if contract_id:
-                self.pool.get('sale.order').write(cr, uid, ids, {'education_contract_id': contract_id})
-                self.pool.get('education_contract.contract').write(cr, uid, [contract_id],
-                                                                   {'campus_id':
-                                                                        sale_o.operating_unit_id.id}, context=context)
-
+                self.pool.get('sale.order').write(
+                    cr,
+                    uid,
+                    ids,
+                    {'education_contract_id': contract_id}
+                )
+                self.pool.get('education_contract.contract').write(
+                    cr,
+                    uid,
+                    [contract_id],
+                    {'campus_id': sale_o.operating_unit_id.id},
+                    context=context
+                )
         return res
 
     def generate_education_contract(self, cr, uid, ids, context=None):
-
-        contract_id = self.pool.get('education_contract.contract').create(cr, uid, {'sale_order_id': ids},
-                                                                          context=context)
+        contract_id = self.pool.get('education_contract.contract').create(
+            cr,
+            uid,
+            {'sale_order_id': ids},
+            context=context
+        )
         return contract_id

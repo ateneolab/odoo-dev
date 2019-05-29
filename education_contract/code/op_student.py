7c4d0f29a845dc181307abff1f55fdd3541a035d
Erich Mario <emgomez@uci.cu>
2019-05-29 16:36:11 -0400

    Valida que no existan dos estudiantes con la misma ced/ruc/pasaport

--- education_contract/code/op_student.py
+++ education_contract/code/op_student.py
@@ -1,17 +1,32 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 import datetime
-
+from openerp.exceptions import ValidationError
 from openerp import models, fields, api, _
 
 
 class Student(models.Model):
-    _name = 'op.student'
-    _inherit = 'op.student'
+    _name = "op.student"
+    _inherit = "op.student"
 
-    school_support = fields.Boolean(u'Apoyo escolar')
-    language_stimulation = fields.Boolean(u'Estimulación del lenguaje')
-    notes = fields.Text(u'Notas relacionadas')
-    social_club = fields.Char(u'Club social')
-    emotional_support = fields.Boolean(u'Apoyo emocional')
-    psico_support = fields.Boolean(u'Apoyo psico pedagógico')
+    school_support = fields.Boolean(u"Apoyo escolar")
+    language_stimulation = fields.Boolean(u"EstimulaciÃ³n del lenguaje")
+    notes = fields.Text(u"Notas relacionadas")
+    social_club = fields.Char(u"Club social")
+    emotional_support = fields.Boolean(u"Apoyo emocional")
+    psico_support = fields.Boolean(u"Apoyo psico pedagÃ³gico")
+
+    @api.model
+    def create(self, vals):
+        self.check_exist_student(vals)
+        res = super(Student, self).create(vals)
+        return res
+
+    def check_exist_student(self, vals):
+        if "ced_ruc" in vals:
+            ced_ruc = vals.get("ced_ruc")
+            count = self.search_count([("ced_ruc", "=", ced_ruc)])
+            if count > 0:
+                raise ValidationError(
+                    "Ya existe un estudiante con esa CÃ©dula/RUC/Pasaporte."
+                )

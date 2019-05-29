5a8c5ec5e8af4ad263f53480bf1345975400015b
Erich Mario <emgomez@uci.cu>
2019-05-29 13:56:13 -0400

    Arreglar formato y hacer depender de invoice_discount_amount
    Este addon se utiliza para reflejar los descuentos en la factura

--- collection_plan/__openerp__.py
+++ collection_plan/__openerp__.py
@@ -1,41 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Collections",
-
-    'summary': """
+    "name": "Collections",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "Solem Consulting",
-    'website': "www.solemconsulting.com",
-
+    "author": "Solem Consulting",
+    "website": "www.solemconsulting.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Collection',
-    'version': '0.1',
-
+    "category": "Collection",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'education_contract', 'fix_discount_type'],
-
+    "depends": ["base", "education_contract"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'templates.xml',
-        'views/collection_plan_view.xml',
-        'views/menu_view.xml',
-        'views/pyment_term_views.xml',
-        'views/sequence_voucher.xml',
-        'wizard/wizard_invoice_view.xml',
-        'wizard/wizard_sale_receipt_view.xml',
-        'report/receipt_report.xml',
+        "templates.xml",
+        "views/collection_plan_view.xml",
+        "views/menu_view.xml",
+        "views/pyment_term_views.xml",
+        "views/sequence_voucher.xml",
+        "wizard/wizard_invoice_view.xml",
+        "wizard/wizard_sale_receipt_view.xml",
+        "report/receipt_report.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
-    ],
+    "demo": ["demo.xml"],
 }
--- education_contract/__openerp__.py
+++ education_contract/__openerp__.py
@@ -1,47 +1,52 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Educational Contract",
-
-    'summary': """
+    "name": "Educational Contract",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "Solem Consulting",
-    'website': "http://www.solemconsulting.com",
-
+    "author": "Solem Consulting",
+    "website": "http://www.solemconsulting.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Education Contract',
-    'version': '0.1',
-
+    "category": "Education Contract",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'account_voucher', 'sale', 'openeducat_erp', 'mail', 'hr', 'advance_salary',
-                'operating_unit', 'oeducat_roll_state', 'scrollable_tree_view', 'oeducat_attendance'],
-
+    "depends": [
+        "base",
+        "account",
+        "account_voucher",
+        "sale",
+        "openeducat_erp",
+        "mail",
+        "hr",
+        "advance_salary",
+        "operating_unit",
+        "oeducat_roll_state",
+        "scrollable_tree_view",
+        "oeducat_attendance",
+        "invoice_discount_amount",
+    ],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'templates.xml',
-        'views/sale_order_view.xml',
-        'views/contract_view.xml',
-        'data/change_menu.xml',
-        'views/menu.xml',
-        'views/student_view.xml',
-        'views/workflow.xml',
-        'views/company_view.xml',
-        'views/conciliation_view.xml',
-        'views/op_roll_number_view.xml',
-        'views/op_student_view.xml',
-        'wizards/report_rpm.xml',
-        'report/view_report.xml',
+        "templates.xml",
+        "views/sale_order_view.xml",
+        "views/contract_view.xml",
+        "data/change_menu.xml",
+        "views/menu.xml",
+        "views/student_view.xml",
+        "views/workflow.xml",
+        "views/company_view.xml",
+        "views/conciliation_view.xml",
+        "views/op_roll_number_view.xml",
+        "views/op_student_view.xml",
+        "wizards/report_rpm.xml",
+        "report/view_report.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
-    ],
+    "demo": ["demo.xml"],
 }

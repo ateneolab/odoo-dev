e49e0e3811f2bf511cf548e0873686e4f0b4ecb3
Erich Mario <emgomez@uci.cu>
2019-06-28 12:07:15 -0400

    Pasar el computado para cobranza
    En caso de que el congelamiento sea a partir de una cobranza no se
    calcula la fecha de reingreso, toma la que viene de cobranza.
    Esto me permite poder actualizar la fecha de reingreso cuando la cobranza
    sale del estado congelado.

--- collection_plan/code/op_roll_number.py
+++ collection_plan/code/op_roll_number.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+import datetime
+from dateutil.relativedelta import relativedelta
+
+from openerp import models, fields, api
 
 
 class Freeze(models.Model):
@@ -9,3 +12,18 @@ class Freeze(models.Model):
     is_collection = fields.Boolean(
         help="Identifica si el congelamiento fue realizado por una cobranza."
     )
+
+    @api.one
+    @api.depends("start_date", "duration")
+    def _compute_end_date(self):
+        """En caso de que el congelamiento sea a partir de una cobranza no se
+        calcula la fecha de reingreso, toma la que viene de cobranza.
+        Esto me permite poder actualizar la fecha de reingreso cuando la cobranza
+        sale del estado congelado.
+        """
+        if not self.is_collection:
+            if self.start_date and self.duration:
+                before_date = datetime.datetime.strptime(self.start_date, "%Y-%m-%d")
+                self.end_date = before_date + relativedelta(months=self.duration)
+            else:
+                self.end_date = False
--- education_contract/code/op_roll_number.py
+++ education_contract/code/op_roll_number.py
@@ -7,41 +7,36 @@ from openerp import models, fields, api, _
 
 
 class RollNumber(models.Model):
-    _name = 'op.roll.number'
-    _inherit = 'op.roll.number'
+    _name = "op.roll.number"
+    _inherit = "op.roll.number"
 
-    beneficiary_id = fields.Many2one('education_contract.beneficiary', string=_('Roll'))
-    schedule_reservation_date = fields.Date(u'Fecha de separación de horario')
-    start_date = fields.Date(u'Fecha de inicio de clases')
-    end_date = fields.Date(u'Fecha de terminación')  # , compute='compute_end_date'
-    diploma_date = fields.Date(u'Fecha de entrega de título')
-    frozen = fields.Boolean(u'Congelado')
-    freezing_ids = fields.One2many('op.roll.number.freeze', 'roll_number_id', u'Congelamientos')
+    beneficiary_id = fields.Many2one("education_contract.beneficiary", string=_("Roll"))
+    schedule_reservation_date = fields.Date(u"Fecha de separación de horario")
+    start_date = fields.Date(u"Fecha de inicio de clases")
+    end_date = fields.Date(u"Fecha de terminación")  # , compute='compute_end_date'
+    diploma_date = fields.Date(u"Fecha de entrega de título")
+    frozen = fields.Boolean(u"Congelado")
+    freezing_ids = fields.One2many(
+        "op.roll.number.freeze", "roll_number_id", u"Congelamientos"
+    )
 
     @api.model
     def create(self, vals):
-        if not 'start_date' in vals:
-            vals.update({
-                'start_date': datetime.datetime.today(),
-                # 'active': True
-            })
+        if not "start_date" in vals:
+            vals.update(
+                {
+                    "start_date": datetime.datetime.today(),
+                    # 'active': True
+                }
+            )
         res = super(RollNumber, self).create(vals)
         return res
 
 
 class Freeze(models.Model):
-    _name = 'op.roll.number.freeze'
+    _name = "op.roll.number.freeze"
 
-    start_date = fields.Date(u'Fecha de inicio')
-    end_date = fields.Date(u'Fecha fin', compute='_compute_end_date')
-    duration = fields.Integer(u'Duración en meses')
-    roll_number_id = fields.Many2one('op.roll.number', u'Matrícula')
-
-    @api.one
-    @api.depends('start_date', 'duration')
-    def _compute_end_date(self):
-        if self.start_date and self.duration:
-            before_date = datetime.datetime.strptime(self.start_date, '%Y-%m-%d')
-            self.end_date = before_date + relativedelta(months=self.duration)
-        else:
-            self.end_date = False
+    start_date = fields.Date(u"Fecha de inicio")
+    end_date = fields.Date(u"Fecha fin", compute="_compute_end_date")
+    duration = fields.Integer(u"Duración en meses")
+    roll_number_id = fields.Many2one("op.roll.number", u"Matrícula")

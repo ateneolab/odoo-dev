19ddc6be98d9af598c5ffabf5d51e055601969da
Erich Mario <emgomez@uci.cu>
2019-07-04 08:02:34 -0400

    Arreglar el error de permiso en las sucursales

--- education_contract/code/operating_unit.py
+++ education_contract/code/operating_unit.py
@@ -7,20 +7,7 @@ class OperatingUnit(models.Model):
 
     _name = "operating.unit"
     _inherit = "operating.unit"
-    _inherits = {"ir.sequence": "ir_sequence"}
 
     ir_sequence = fields.Many2one(
-        "ir.sequence",
-        string="Sequence",
-        delegate=True,
-        required=True,
-        ondelete="cascade",
+        "ir.sequence", string="Sequence", delegate=True, ondelete="cascade"
     )
-
-    @api.model
-    def create(self, vals):
-        name = vals["name"]
-        seq = self.env["ir.sequence"].create(dict(name=name))
-        vals["ir_sequence"] = seq.id
-        res = super(OperatingUnit, self).create(vals)
-        return res
